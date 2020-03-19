133b96c8510efc5015bbbea6c6f6494277f2c5fc
sasukeofficielgfdgfdgdfgdfgdfgdfgd <62369494+sasukeofficielgfdgfdgdfgdfgdfgdfgd@users.noreply.github.com>
2020-03-19 05:49:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const client = new Discord.Client();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+ 
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({ description: [], credits: [], note: [], rep: [], age: []}).write()
+
+const config = require("./config.json");
+client.config = config;
+
+var now = new Date();
+var hour = now.getHours();
+var minute = now.getMinutes();
+var second = now.getSeconds();
+var times = (`[${hour}:${minute}:${second}]/`);
+
+client.on('ready', () => {
+  console.log(times+`\x1b[33m%s\x1b[0m`,'[WARN]','\x1b[0m','Connexion en cours...');
+  console.log(times+`\x1b[33m%s\x1b[0m`,'[WARN]','\x1b[0m','Connexion à l\'API Discord.js en cours...');
+  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m', 'Connexion à l\'API Discord.js effectuée');
+  console.log(times+`\x1b[36m%s\x1b[0m`,'[INFO]', '\x1b[0m','Connecté sur ' + client.user.username + '#' + client.user.discriminator);
+  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m','Chargement terminé');
+  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m','Prêt et connecté');
+
+  const activities = [
+      "//help | Verticals",
+      `//help | ${client.guilds.size} serveurs`,
+      `//help | ${client.users.size} users`
+  ];
+  client.setInterval(() => {
+      const index = Math.floor(Math.random() * activities.length);
+      client.user.setActivity(activities[index], {
+          type: "PLAYING",
+          url: "http://twitch.tv/Moon"
+      });
+  }, 15000);
+});
+
+
+client.login(config.token);
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Discord.Collection();
+
+// Recherche de toutes les commandes
+fs.readdir("./commands/", (err, content) => {
+  if(err) console.log(err);
+  if(content.length < 1) return console.log('Veuillez créer des dossiers dans le dossier commands !');
+  var groups = [];
+  content.forEach(element => {
+      if(!element.includes('.')) groups.push(element); // Si c'est un dossier
+  });
+  groups.forEach(folder => {
+      fs.readdir("./commands/"+folder, (e, files) => {
+          let js_files = files.filter(f => f.split(".").pop() === "js");
+          if(js_files.length < 1) return console.log('Veuillez créer des fichiers dans le dossier "'+folder+'" !');
+          if(e) console.log(e);
+          js_files.forEach(element => {
+              let props = require('./commands/'+folder+'/'+element);
+              client.commands.set(element.split('.')[0], props);
+          });
+      });
+  });
+});
+
+client.on('guildCreate', guild => {
+    const embed = new Discord.RichEmbed()
+        .setDescription(`📌 Merci à **${guild.name}** d'avoir ajouté ${client.user.username}`)
+        .addField("📋 __Nom du serveur__", guild.name, true)
+        .addField("📊 __Nombre de membres__ :", guild.memberCount, true)
+        .addField("💻 __Nombre de salons__ :", guild.channels.size, true)
+        .addField("👤 __Propriétaire__ :", guild.owner, true)
+        .addField("🌍 __Région du serveur__ :", guild.region, true)
+        .addField("📝 __ID du serveur__ :", guild.id, true)
+        .setColor("#F03A17")
+      client.channels.get('690054474487824410').send(embed);
+});
